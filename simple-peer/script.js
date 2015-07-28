6fa87895ac2e842e4bf1d518d0c1a862189509d7
unhandyandy <unhandyandy@gmail.com>
2015-07-28 14:43:00 -0400

    Update script.js

--- simple-peer/script.js
+++ simple-peer/script.js
@@ -1,16 +1,30 @@
 var Peer = window.SimplePeer
 var p = new Peer({ initiator: location.hash === '#1', trickle: false })
+var invitation
+var stage = 0
 
 p.on('error', function (err) { console.log('error', err) })
 
+function sendMail() {
+    var link = "mailto:" + escape(document.getElementById('incoming').value)
+             + "&subject=" + escape("Game Invitation")
+             + "&body=" + JSON.stringify(invitation) );
+    window.location.href = link; }
+
 p.on('signal', function (data) {
   console.log('SIGNAL', JSON.stringify(data))
-  document.querySelector('#outgoing').textContent = JSON.stringify(data)
+  invitation = data
 })
-
+document.querySelector('#incoming').addEventListener('input', function (ev) {
+  stage = 0
+})
 document.querySelector('form').addEventListener('submit', function (ev) {
   ev.preventDefault()
-  p.signal(JSON.parse(document.querySelector('#incoming').value))
+  if (stage > 0){
+    p.signal(JSON.parse(window.clipboardData.getData('Text'))) 
+  }else{
+    sendMail()
+    stage += 1}
 })
 
 document.querySelector('#type').addEventListener('keypress', function (ev) {
@@ -28,5 +42,5 @@ p.on('connect', function () {
 
 p.on('data', function (data) {
   console.log('data: ' + data)
-  document.querySelector('#thread').value += "\n\n" + data
+  document.querySelector('#thread').innerHTML += "<br/><br/>" + data
 })
