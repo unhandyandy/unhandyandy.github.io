843eebc5d32c1030f44da7ad70b4f04bde4ea288
unhandyandy <unhandyandy@gmail.com>
2015-07-29 11:27:22 -0400

    Update script.js

--- simple-peer/script.js
+++ simple-peer/script.js
@@ -7,8 +7,8 @@ p.on('error', function (err) { console.log('error', err) })
 
 function sendMail() {
     var link = "mailto:" + escape(document.getElementById('incoming').value) +
-               "&subject=" + escape("Game Invitation") +
-               "&body=" + JSON.stringify(invitation);
+               "?subject=" + escape("Game Invitation") +
+               "?body=" + JSON.stringify(invitation);
     window.location.href = link; }
 
 p.on('signal', function (data) {
